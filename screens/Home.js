9d89c9ef2426e88a7cb1f09fe4ff690c21db32f5
jay <pateljay8886@gmail.com>
2024-03-22 12:56:44 +0530

    Home updated

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,25 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  Image,
+} from "deprecated-react-native-prop-types";
 import React from "react";
 import { Icon } from "react-native-vector-icons/FontAwesome";
 import { SliderBox } from "react-native-image-slider-box";
-const image = [
+const images1 = [
   require("../assets/1.jpg"),
   require("../assets/2.jpg"),
   require("../assets/3.jpg"),
+];
+const items = [
+  { id: 1, image: require("../assets/11.jpg"), text: "Top" },
+  { id: 2, image: require("../assets/22.jpg"), text: "Jeans" },
+  { id: 3, image: require("../assets/33.jpg"), text: "Shoes" },
+  { id: 4, image: require("../assets/44.jpg"), text: "Jacket" },
+  { id: 5, image: require("../assets/55.jpg"), text: "Cap" },
 ];
 export default function Home({ navigation }) {
   return (
@@ -13,15 +27,14 @@ export default function Home({ navigation }) {
       <View style={styles.topC}>
         <Text style={styles.topT}>Home</Text>
       </View>
-      
+      <ScrollView showsVerticalScrollIndicator={false}>
       <View style={styles.Container}>
         <View style={styles.searchContainer}>
-
           <TextInput style={styles.searchInput} placeholder="Search" />
         </View>
       </View>
       <SliderBox
-        images={image}
+        images={images1}
         style={styles.carousel}
         dotColor="#476DFF"
         inactiveDotColor="#ffffff"
@@ -31,6 +44,23 @@ export default function Home({ navigation }) {
       <View style={styles.mTextView}>
         <Text style={styles.mText}>Categories</Text>
       </View>
+      <View>
+        <ScrollView
+          contentContainerStyle={styles.scrollViewContent}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        >
+          {items.map((item) => (
+            <View key={item.id} style={styles.itemContainer}>
+              <View style={styles.imageContainer}>
+                <Image source={item.image} style={styles.image} />
+              </View>
+              <Text>{item.text}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -60,7 +90,7 @@ const styles = StyleSheet.create({
     borderWidth: 1.1,
     borderRadius: 8,
     width: "90%",
-    height:43,
+    height: 43,
     marginTop: "5%",
   },
   carousel: {
@@ -77,5 +107,30 @@ const styles = StyleSheet.create({
   mTextView: {
     marginLeft: "7.2%",
     marginTop: 10,
+  },
+
+  scrollViewContent: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    padding: 10,
+  },
+  itemContainer: {
+    alignItems: "center",
+    marginLeft: 20,
+  },
+  image: {
+    width: 55,
+    height: 55,
+    borderRadius: 50, // Make it a circle
+  },
+  imageContainer: {
+    borderRadius: 50, // Make it a circle
+    overflow: "hidden",
+    elevation: 5, // Shadow on Android
+    shadowColor: "black", // Shadow color
+    shadowOffset: { width: 0, height: 2 }, // Shadow offset
+    shadowOpacity: 0.3, // Shadow opacity
+    shadowRadius: 4, // Shadow radius
   },
 });
