36a3b9d99ebb341482ebea1ba587501ad10376f2
Jay <81481348+jaypatelll1@users.noreply.github.com>
2024-03-25 22:33:03 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
-const port = 8000;
 
+const PORT= process.env.PORT || 8000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -28,8 +28,8 @@ mongoose.connect("mongodb+srv://urbanutopia:urbanutopia@cluster0.tbg46b5.mongodb
     console.log("Error ", err);
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
 const sendVerificationEmail=async(email,verificationToken)=>{
     const transporter=nodemailer.createTransport({
