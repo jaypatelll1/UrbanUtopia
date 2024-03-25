8c33fd2dcb1760bcbc0515c7340e24365988d27c
Mihir <mihirvora1053@gmail.com>
2024-03-25 14:40:03 +0530

    Redux Setup Done

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import {  View } from 'react-native';
+import { View } from "react-native";
 import Appnav from "./navigation/Appnav";
-import Register from "./screens/Register"
-import Profile from './screens/Profile';
-import 'react-native-gesture-handler';
-import { LogBox } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import Test1 from './screens/Test1';
-LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
-LogBox.ignoreAllLogs();//Ignore all log notifications
+import Register from "./screens/Register";
+import Profile from "./screens/Profile";
+import "react-native-gesture-handler";
+import { LogBox } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Test1 from "./screens/Test1";
+import ProductInfo from "./screens/ProductInfo";
+import { Provider } from "react-redux";
+import store from "./store";
+LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 export default function App() {
   return (
-    <NavigationContainer >
-      <Appnav/>
-      {/* <Test1/> */}
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Appnav />
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-
-
